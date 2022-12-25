629763a120cb93700c52d57ca623d23887b06597
Beksultan <beksultan.sagnaev@gmail.com>
2022-12-25 13:12:16 +0600

    feat: localstorage add item and initiate items

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ let addItem = document.querySelector('.add-item'),
     newItem,
     searchVal,
     parentDiv = document.querySelector('.tasks'),
-    searchInput = document.querySelector('.search')
+    searchInput = document.querySelector('.search'),
+    storageArr = []
+    
 
 searchInput.addEventListener('input', function(event) {
     searchVal = event.target.value;
@@ -24,60 +26,6 @@ addItem.addEventListener('keypress', function(e) {
 addBtn.addEventListener('click', function(e) {
     newTodo()
 })
-
-function newTodo() {
-    let newEl = document.createElement('div'),
-        text = document.createElement('div'),
-        deleteBtn = document.createElement('img'),
-        starBtn = document.createElement('img'),
-        icons = document.createElement('div'),
-        checkboxBtn = document.createElement('input')
-
-    newEl.className = 'task'
-    text.className = 'task-text'
-    text.innerHTML = newItem[0].toUpperCase() + newItem.slice(1)
-
-    icons.className = 'btns'
-    deleteBtn.src = './trash-can-solid.svg'
-    deleteBtn.className = 'delete'
-
-    starBtn.src = './star-solid.svg'
-    starBtn.className = 'star'
-
-    checkboxBtn.type = 'checkbox'
-    checkboxBtn.className = 'complete'
-
-    newEl.appendChild(checkboxBtn)
-    newEl.appendChild(text)
-    icons.appendChild(deleteBtn)
-
-    icons.appendChild(starBtn)
-    newEl.appendChild(icons)
-
-    theFirsrtChild = parentDiv.firstChild
-    parentDiv.insertBefore(newEl, theFirsrtChild)
-    addItem.value = ''
-
-    bindTaskEvents(newEl)
-}
-
-let bindTaskEvents = function(item) {
-    // let deleteBtn
-    // if(item.length > 1) item.forEach(it => deleteBtn = it.querySelector('.delete'))
-    // console.log(item);
-    // else {}
-    starBtn = item.querySelector('.star')
-    deleteBtn = item.querySelector('.delete')
-    checkboxBtn = item.querySelector('.complete')
-
-    starBtn.onclick = importantTask
-    deleteBtn.onclick = deleteTask
-    checkboxBtn.onchange = taskComplete
-    // else if(checkboxBtn.value === 'off') checkboxBtn.onchange = inCompletetask
-}
-//bind - связать
-
-// true, false
 
 let taskComplete = function(e) {
 
@@ -113,6 +61,81 @@ let deleteTask = function() {
     let ul = li.parentNode //task -> tasks
     ul.removeChild(li) //tasks (task)
 }
+
+let bindTaskEvents = function(item) {
+    // let deleteBtn
+    // if(item.length > 1) item.forEach(it => deleteBtn = it.querySelector('.delete'))
+    // console.log(item);
+    // else {}
+    starBtn = item.querySelector('.star')
+    deleteBtn = item.querySelector('.delete')
+    checkboxBtn = item.querySelector('.complete')
+
+    starBtn.onclick = importantTask
+    deleteBtn.onclick = deleteTask
+    checkboxBtn.onchange = taskComplete
+    // else if(checkboxBtn.value === 'off') checkboxBtn.onchange = inCompletetask
+}
+
+function runInitial() {
+    initialTasks = JSON.parse(localStorage.getItem("tasks"))
+    initialTasks.map((task) => {
+        newTodo(task)
+    })
+}
+
+runInitial()
+
+function newTodo(task) {
+    
+    console.log(task);
+    let newEl = document.createElement('div'),
+        text = document.createElement('div'),
+        deleteBtn = document.createElement('img'),
+        starBtn = document.createElement('img'),
+        icons = document.createElement('div'),
+        checkboxBtn = document.createElement('input')
+
+    newEl.className = 'task'
+    text.className = 'task-text'
+
+    text.innerHTML = task? task: newItem[0].toUpperCase() + newItem.slice(1)
+
+    storageArr.push(task? task : newItem[0].toUpperCase() + newItem.slice(1))
+    localStorage.setItem('tasks', JSON.stringify(storageArr))
+
+
+    icons.className = 'btns'
+    deleteBtn.src = './trash-can-solid.svg'
+    deleteBtn.className = 'delete'
+
+    starBtn.src = './star-solid.svg'
+    starBtn.className = 'star'
+
+    checkboxBtn.type = 'checkbox'
+    checkboxBtn.className = 'complete'
+
+    newEl.appendChild(checkboxBtn)
+    newEl.appendChild(text)
+    icons.appendChild(deleteBtn)
+
+    icons.appendChild(starBtn)
+    newEl.appendChild(icons)
+
+    theFirsrtChild = parentDiv.firstChild
+    parentDiv.insertBefore(newEl, theFirsrtChild)
+    addItem.value = ''
+
+    bindTaskEvents(newEl)
+
+}
+
+
+//bind - связать
+
+// true, false
+
+
 
 let search = function(value) {
     console.log('Поиск...', value);
